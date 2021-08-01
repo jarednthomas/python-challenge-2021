c9a65530e4b837ddbc3fd26564515972f6481f26
Jared Thomas <jaredpyrothomas@gmail.com>
2021-07-31 23:18:25 -0500

    clean solution

--- 2.py
+++ 2.py
@@ -10,7 +10,7 @@ headers = {
     'Access-Control-Allow-Headers': 'Content-Type',
     'Access-Control-Max-Age': '3600',
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
-    }
+}
 
 # Parse html with bs4
 response = requests.get(url, headers)
@@ -20,20 +20,19 @@ soup = BeautifulSoup(response.content, 'html.parser')
 comments = soup.find_all(text=lambda text: isinstance(text, Comment))
 last_comment = comments[-1]
 
-counts = {}
 # Iterate through last comment, counting instances of each char
+counts = {}
 for char in last_comment:
     if char not in counts:
         counts[char] = 1
     else:
         counts[char] += 1
 
-solution = []
 # Iterate through counts, storing values of 1
+solution = []
 for key in counts:
     if counts[key] == 1:
         solution.append(key)
 
 # Return answer url
-answer = "".join(solution)
-print(url.replace("ocr", answer))
+print(url.replace("ocr", "".join(solution)))
