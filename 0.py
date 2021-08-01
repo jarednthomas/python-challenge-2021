42c27496cfbd4c70a289bdb50506aa0a211b81bb
Jared Thomas <jaredpyrothomas@gmail.com>
2021-07-31 22:53:07 -0500

    reformated solution

--- 0.py
+++ 0.py
@@ -1,3 +1,2 @@
-# Next Url
-url = "http://www.pythonchallenge.com/pc/def/0.html"
-print(url.replace("0", str(pow(2, 38))))
+# Replace 0 in the url with 2 to the power of 38
+print("http://www.pythonchallenge.com/pc/def/0.html".replace("0", str(pow(2, 38))))
