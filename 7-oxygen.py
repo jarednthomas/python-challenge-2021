6fcacf63b459a31867365602b34d79114c3ada23
Jared <jaredpyrothomas@gmail.com>
2021-08-01 05:14:38 -0500

    clean solution

--- 7-oxygen.py
+++ 7-oxygen.py
@@ -4,6 +4,7 @@ from PIL import Image
 
 url = "http://www.pythonchallenge.com/pc/def/oxygen.html"
 
+'''
 # Open image resource and save locally
 resource = urllib.request.urlopen("http://www.pythonchallenge.com/pc/def/oxygen.png")
 output = open("7.png","wb")
@@ -17,14 +18,13 @@ with Image.open("7.png") as im:
     # Get dimensions of image and set y to midpoint
     w, h = im.size
 
-    ''' Solution
     # Join the red pixel values as chars within the middle of the image (minus 3ish blocks)
     print("".join( [chr(im.getpixel((i, h//2))[0]) for i in range(0, (w-21), 7)] ) )
 
     # Above hint gives another list, which can be mapped as chars to say "integrity"
     print("".join(map(chr, [105, 110, 116, 101, 103, 114, 105, 116, 121])))
-    '''
+'''
 
-    # Return solution url
-    print(url.replace("oxygen", "integrity"))
+# Return solution url
+print(url.replace("oxygen", "integrity"))
     
